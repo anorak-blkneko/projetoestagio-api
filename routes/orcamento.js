5e17cee6a99d7067b05251947e0dc16dbdaa99e6
BLKNeko <blackheart2302@gmail.com>
2021-10-25 14:26:02 -0300

    test 2

--- routes/orcamento.js
+++ routes/orcamento.js
@@ -100,7 +100,7 @@ router.get('/relcli/:id', (req, res, next) =>{
 //RETORNA O ORCAMENTO CONCLUIDO
 router.get('/relcon/', (req, res, next) =>{
 
-    client.query(`Select * from orcamento where status_andamento=${'CONCLUIDO'}`, (err, result)=>{
+    client.query(`Select * from orcamento where status_andamento=CONCLUIDO`, (err, result)=>{
         if(!err){
             res.send(result.rows);
         }
